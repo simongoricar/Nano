0c2b0d1b419a15471eae38106566465c52678635
DefaltSimon <simon.peter.goricar@gmail.com>
2016-01-02 15:21:13 +0100

    0.9
    - Moved settings to settings.ini
    - Added config.py "module" where you add your commands

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
-__name__ = "DiscordieBot simple commands list"
-
-
-'''
-Adding commands:
-
-Just add them with the help of a template:
-
-"<yourtriggercommand>": "<whatYouWantToPrint>",
-                                              ^ everywhere except the last line
-
-The second can include <@{usr}> to mention the author.
-Use \n anywhere for new line in the same message.
-'''
+__name__ = "AyyBot simple commands list"
 
 things = {
     "!hello": "Hi, <@{usr}>",
@@ -25,4 +12,22 @@ things = {
     "!synagoge": "DIE ALTEE-SYNAGOGE",
     "!thecakeisalie": "<@{usr}> : Rick roll'd https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "!who": "<@{usr}> Can't manipulate strings. Not yet. Soon."
-    }
+}
+'''
+settings.ini :
+
+FilterWords: 1 or 0 (filters words in filterwords.txt)
+FilterSpam: 1 or 0 (filters spam)
+WriteLogs: 1 or 0 (writes logs to log.txt)
+
+
+Adding commands:
+
+Just add them with the help of a template:
+
+"<yourtriggercommand>": "<whatYouWantToPrint>",
+                                              ^ everywhere except the last line
+
+The second can include <@{usr}> to mention the author.
+Use \n anywhere for new line in the same message.
+'''
--- discord-bot.py
+++ discord-bot.py
@@ -1,25 +1,29 @@
+import discord
+import os
+from random import randint
+from config import things
+from datetime import timedelta, datetime
+import time
+import configparser
+
 __title__ = 'DiscordieBot'
 __author__ = 'DefaltSimon with discord.py api'
-__version__ = '0.8'
+__version__ = '0.9'
 
-import discord
-import time
-from random import randint
-from datetime import datetime, timedelta
-from time import strftime
-from scommands import *
-import os
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
 client = discord.Client()
 clientchannel = discord.Channel()
-botvs = "0.8"
+botvs = "0.9"
+parser = configparser.ConfigParser()
+parser.read("settings.ini")
 
 def runme():
     client.run()
 
 def loginme():
     client.login('mail', 'pass')
+loginme()
 
 def logmeout():
     client.logout()
@@ -30,15 +34,20 @@ def deletemsg(message):
 def printout(message, disauthor):
     print('{msg} was executed by {usr}'.format(msg=str(message.content), usr=disauthor))
 
-loginme()
-# TIME
-start_time = time.time()
-
 def Gettime(time_elapsed):
     sec = timedelta(seconds=time_elapsed)
     d = datetime(1, 1, 1) + sec
     this = "%d:%d:%d:%d" % (d.day - 1, d.hour, d.minute, d.second)
     return this
+
+
+def logdis(message):
+    if parser.getboolean("SettingsOne", "WriteLogs") == 1:
+        with open('log.txt',"a") as file2:
+            one = str(time.strftime("%Y-%m-%d %H:%M:%S", ))
+            str2 = one + str(" : {msg} by {usr}\n".format(msg=message.content,usr=message.author))
+            file2.write(str2)
+
 
 def checkwords(message):
     cmsg = str(message.content).strip()
@@ -47,6 +56,7 @@ def checkwords(message):
     for bads in mylist:
         if bads in cmsg and message.author != "AyyBot":
             client.send_message(message.channel, "@{usr}, watch it!".format(usr=message.author))
+
 
 def checkspam(message):
     spamword = ""
@@ -65,33 +75,12 @@ def checkspam(message):
                             "@{usr} Spam is not allowed. **Deal with it** ( ͡° ͜ʖ ͡°)".format(usr=message.author))
         print("A message by {usr} was filtered - spam".format(usr=message.author))
 
-def checksettings(parm):
-    with open('settings.txt') as a:
-        appsettings = [line.rstrip('\n') for line in a]
-    if parm == 1:
-        if appsettings[0] == "FilterWords: yes":
-            return 1
-        else:
-            return 0
-    if parm == 2:
-        if appsettings[1] == "FilterSpam: yes":
-            return 1
-        else:
-            return 0
-
-with open('log.txt','a') as file:
-    file.write("\n------------------------------\n".format(vs=botvs))
-
-def logdis(message):
-    with open('log.txt',"a") as file:
-        one = str(strftime("%Y-%m-%d %H:%M:%S", ))
-        str2 = one + str(" : {msg} by {usr}".format(msg=message.content,usr=message.author))
-        print(str2)
-        file.write(str2)
+# TIME
+start_time = time.time()
 
 helpmsg1 = ("""\
 Help:
-!help 1 - displays available commads (almost all)
+!help 1 - displays available commands (almost all)
 !help 2 - funny commands
 !hello - says hi
 !roll - random number between 0 and 100
@@ -99,52 +88,54 @@ Help:
 !listmembers - displays all members on the server
 !uptime - displays bot uptime
 !getinvite - returns an invite for the server/channel
-!avatar ""@<usr>"" or "me" - returns a link to your/mentioned person's avatar
+!avatar ""@<usr>"" returns a link to mentioned person's avatar or yours if there is no mention
 !restart - restarts the bot
 !credits - author, etc.
 """)
 creditsmsg = ("""\
-**DiscordBot 0.8**
+**DiscordBot 0.9**
 Made by *DefaltSimon* with the help of \"discord.py\" API(on github).
 """)
 jokemsg = ("""\
-**Help, page 2:**
-!johncena - meme
-!allstar - all star song
-!game - mentions everyone and asks about playing a game
+**Help, fun commands:**
 !ayy - ayy lmao
-!moreayy - even more ayy with lenny
-!wot - idk why I added this
-!synagoge - just a friend's joke
-!thecakeisalie - get rick roll'd
-!cats - aren't they cute?
+!moreayy - even more ayy lmao mit dem lenny face
+!wot - u wot m8
+!synagoge - much joke, such laugh
+!allstar just try it ( ͡° ͜ʖ ͡°)
+!johncena dis too ( ͡° ͜ʖ ͡°)
+!thecakeisalie - want it?
 """)
 
 @client.event
 def on_message(message):
     try:
         disauthor = message.author
-        # Spam and swearing check / with settings.txt check in checksettings(parm=1 or 2)
-        if checksettings(parm=1) == 1:
+        # Spam and swearing check / with settings.txt check in config.checksettings(parm=1 or 2)
+        if parser.getboolean("SettingsOne", "FilterWords") == 1:
             checkwords(message)
-        if checksettings(parm=2) == 1:
+        if parser.getboolean("SettingsOne", "FilterSpam") == 1:
             checkspam(message)
-        # commands imported from scommands.py
+        # commands imported from config.py
         for onething in things.keys():
             if message.content.startswith(onething):
                 deletemsg(message)
                 second = things.get(str(message.content))
+                print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
                 logdis(message)
                 client.send_message(message.channel, second.format(usr=disauthor.id))
         # Help
         if message.content.startswith("!help"):
             client.send_message(message.channel, helpmsg1)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         elif message.content.startswith('!help 1'):
             client.send_message(message.channel, helpmsg1)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         elif message.content.startswith("!help 2"):
             client.send_message(message.channel, jokemsg)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         # Restart // will add permission check
         elif message.content.startswith("!restart"):
@@ -154,30 +145,38 @@ def on_message(message):
             runme()
             os.system('clear')
             print("Should be properly restarted.")
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         # They see me rollin' // kinda works
         elif message.content.startswith('!roll'):
             deletemsg(message)
             client.send_message(message.channel, "<@" + disauthor.id + "> rolled :" + randint(0, 100))
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         elif message.content.startswith("!dice"):
             deletemsg(message)
             client.send_message(message.channel, "<@" + disauthor.id + "> You got: " + str(randint(0,6)) + "!")
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
+            logdis(message)
         # Credits.
         elif message.content.startswith("!credits"):
             deletemsg(message)
             client.send_message(message.channel, creditsmsg)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         # Shut down. // to do : add check for permissions.
         elif message.content.startswith("!shutmedown"):
             deletemsg(message)
             client.send_message(message.channel, "<@" + disauthor.id + ">, DiscordieBot shutting down.")
-            exit(-420)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
+            exit(-420)
         # Cats are cute
         elif message.content.startswith("!cats"):
             deletemsg(message)
             client.send_file(message.channel, "cattypo.gif")
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
+            logdis(message)
             printout(message, disauthor)
         # Lists all members in current server
         elif message.content.startswith("!listmembers"):
@@ -186,7 +185,7 @@ def on_message(message):
             count = 0
             members = ''
             for mem in client.get_all_members():
-                count = count + 1
+                count += 1
                 mem = str(mem)
                 if count != 1:
                     members += ', '
@@ -194,6 +193,7 @@ def on_message(message):
             final = "**Total : {count1} members.**".format(count1=count)
             client.send_message(message.channel, members)
             client.send_message(message.channel, final)
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         # Uptime
         elif message.content.startswith("!uptime"):
@@ -201,20 +201,18 @@ def on_message(message):
             time_elapsed = time.time() - start_time
             converted = Gettime(time_elapsed)
             client.send_message(message.channel, "<@" + disauthor.id + "> **Uptime: {upt} **".format(upt=converted))
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
         # Gets you an invite // doesnt work yet
         elif message.content.startswith("!getinvite"):
             disinvite = client.create_invite(message.server)
             deletemsg(message)
-            client.send_message(message.channel,
-                                "<@{usr}>, here is your code : {code} that expires in 24 hrs.".format(usr=disauthor.id,code=str(disinvite)))
+            client.send_message(message.channel,"<@{usr}>, here is your code : {code} that expires in 24 hrs.".format(usr=disauthor.id,code=str(disinvite)))
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
-        # If followed by "me", returns yours, otherwise mentioned user's avatar
+        # Returns mentioned user's avatar, or if no mention present, yours.
         elif message.content.startswith("!avatar"):
             deletemsg(message)
-            if message.content.startswith("!avatar me"):
-                user2 = message.author
-                client.send_message(message.channel,"<@" + disauthor.id + ">'s avatar: " + user2.avatar_url())
             if len(message.mentions) > 0:
                 for user in message.mentions:
                     if user.avatar_url() != "":
@@ -222,10 +220,10 @@ def on_message(message):
                     else:
                         client.send_message(message.channel, user.name + " doesn't have an avatar.")
             else:
-                if message.content.startswith() != "!avatar me":
-                    client.send_message(message.channel, message.author.mention() + " Mention the users you want to get avatars from.")
+                user2 = message.author
+                client.send_message(message.channel,"<@" + disauthor.id + ">'s avatar: " + user2.avatar_url())
+            print(str("{msg} by {usr}".format(msg=message.content,usr=message.author)))
             logdis(message)
-            ####End of commands###
     except discord.InvalidArgument:
         print("Error -3 : InvalidArgument")
         client.send_message(message.channel, "Error -3 : InvalidArgument")
@@ -262,5 +260,9 @@ def on_member_join(member):
     server = member.server
     client.send_message(server, 'Welcome to the server, {0}'.format(member.mention()))
     logdis("{one} joined the server".format(one=member))
+
+if parser.getboolean("SettingsOne","WriteLogs") == 1:
+    with open('log.txt','a') as file:
+        file.write("\n------------------------------\n".format(vs=botvs))
 
 runme()
