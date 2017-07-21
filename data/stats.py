f69f1dba236e866936c9e0b229aa1be32bc2712e
DefaltSimon <simon.peter.goricar@gmail.com>
2017-07-21 22:40:59 +0200

    Removed legacy support

--- data/stats.py
+++ data/stats.py
@@ -4,8 +4,8 @@ import os
 import importlib
 import configparser
 import logging
-# yaml is now a conditional import
-from .utils import threaded, decode, decode_auto
+# yaml is a conditional import
+from .utils import threaded, decode
 
 __author__ = "DefaltSimon"
 # Stats handler for Nano
@@ -106,63 +106,3 @@ class RedisNanoStats:
     def _set_amount(self, stat_type: str, amount: int):
         if stat_type in stat_types:
             self.redis.hset("stats", stat_type, amount)
-
-
-class LegacyNanoStats:
-    def __init__(self):
-        self.yaml = importlib.import_module("yaml")
-
-        log.info("Enabled: legacy")
-        self.data_lock = False
-
-        with open("data/stats.yml", "r") as file:
-            self.cached_data = self.load(file)
-
-    def load(self, *args, **kwargs):
-        return self.yaml.load(*args, **kwargs)
-
-    def dump(self, *args, **kwargs):
-        return self.yaml.dump(*args, **kwargs)
-
-    # The locking system
-    def lock(self):
-        self.data_lock = True
-
-    def wait_until_release(self):
-        while self.data_lock is True:
-            time.sleep(0.05)
-
-        return
-
-    def release_lock(self):
-        self.data_lock = False
-
-    @threaded
-    def write(self):
-        # Prevents data corruption
-        self.wait_until_release()
-
-        self.lock()
-        with open("data/stats.yml", "w") as outfile:
-            outfile.write(self.dump(self.cached_data, default_flow_style=False))
-
-        self.release_lock()
-
-    def get_data(self):
-        return self.cached_data
-
-    def _reload_data(self):
-        log.info("Reloading stats from file")
-        with open("data/stats.yml", "r") as file:
-            self.cached_data = self.load(file)
-
-    def get_amount(self, typ):
-        return self.cached_data.get(typ)
-
-    # The code efficiency is getting real
-    def add(self, stat_type):
-        if stat_type in [MESSAGE, WRONG_ARG, SERVER_LEFT, SLEPT, WRONG_PERMS, HELP, IMAGE_SENT, VOTE, PING, SUPPRESS, DOWNLOAD, PRAYER]:
-
-            self.cached_data[stat_type] += 1
-
-            self.write()
