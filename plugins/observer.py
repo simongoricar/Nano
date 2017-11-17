c569c364d283c95d64536de44c4c12f3f90f03bf
DefaltSimon <simon.peter.goricar@gmail.com>
2017-11-17 17:12:31 +0100

    Added rate-limiting (for now: 2 commands per 5 seconds)

--- plugins/observer.py
+++ plugins/observer.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import logging
+import time
 
 from discord import TextChannel
 
@@ -23,12 +24,65 @@ commands = {
 valid_commands = commands.keys()
 
 
-class BotState:
+def get_valid_commands(plugin):
+    try:
+        return list(plugin.commands.keys())
+    except AttributeError:
+        return None
+
+
+class Bucket:
+    __slots__ = ("last_cooldown", "_size", "_cooldown", "current_bucket", "was_warned")
+
+    def __init__(self, limit: int=2, per: int=5):
+        self.last_cooldown = time.time()
+
+        self._size = limit
+        self._cooldown = per
+        self.was_warned = False
+        self.current_bucket = 0
+
+    def action(self):
+        current_time = time.time()
+
+        # If bucket cooldown is reached, reset the bucket
+        if current_time - self.last_cooldown > self._cooldown:
+            self.last_cooldown = current_time
+            self.current_bucket = 0
+            self.was_warned = False
+            return True
+
+        if self.current_bucket >= self._size:
+            return False
+        else:
+            self.current_bucket += 1
+            return True
+
+
+
+class Observer:
     def __init__(self, *_, **kwargs):
         self.client = kwargs.get("client")
         self.handler = kwargs.get("handler")
         self.stats = kwargs.get("stats")
         self.trans = kwargs.get("trans")
+        self.nano = kwargs.get("nano")
+
+        self.buckets = {}
+        self.valid_commands = set()
+
+    async def on_plugins_loaded(self):
+        # Collect all valid commands
+        plugins = [a.get("plugin") for a in self.nano.plugins.values() if a.get("plugin")]
+
+        temp = []
+        for pl in plugins:
+            cmds = get_valid_commands(pl)
+            if cmds is not None:
+                # Joins two lists
+                temp += cmds
+
+        self.valid_commands = set(temp)
 
     async def on_message(self, message, **_):
         trans = self.trans
@@ -36,25 +90,12 @@ class BotState:
         # Ignore your own messages
         if message.author == self.client.user:
             return "return"
-
         # Ignore private messages
         if not isinstance(message.channel, TextChannel):
             return "return"
-
         # Ignore bot messages
         if message.author.bot:
             return "return"
-
-        # Set up the server if it is not present in redis db
-        self.handler.auto_setup_server(message.guild)
-
-        # Ah, the shortcuts
-        def startswith(*matches):
-            for match in matches:
-                if message.content.startswith(match):
-                    return True
-
-            return False
 
         # Add prefix to kwargs for future plugins
         pref = self.handler.get_prefix(message.guild)
@@ -67,6 +108,38 @@ class BotState:
         lang = self.handler.get_lang(message.guild.id)
         if not lang:
             lang = str(self.trans.default_lang)
+
+        # Ignore the filter if user is not executing a command
+        np_text = message.content.replace(pref, "_").split(" ")[0]
+        if np_text in self.valid_commands:
+            # Check rate-limits
+            # If user was silent until now, create a new bucket
+            if message.author.id not in self.buckets.keys():
+                b = Bucket()
+                b.action()
+                self.buckets[message.author.id] = b
+            # Otherwise, check bucket size
+            else:
+                bucket = self.buckets[message.author.id]
+                if not bucket.action():
+                    if not bucket.was_warned:
+                        # Do not send additional warnings in the same time period
+                        bucket.was_warned = True
+                        await message.channel.send(trans.get("MSG_RATELIMIT", lang))
+
+                    return "return"
+
+
+        # Set up the server if it is not present in redis db
+        self.handler.auto_setup_server(message.guild)
+
+        # Ah, the shortcuts
+        def startswith(*matches):
+            for match in matches:
+                if message.content.startswith(match):
+                    return True
+
+            return False
 
         # SLEEP/WAKE Commands!
         # nano.sleep
@@ -157,9 +230,10 @@ class NanoPlugin:
     name = "Prefix and state handler"
     version = "23"
 
-    handler = BotState
+    handler = Observer
     events = {
         "on_message": 4,
+        "on_plugins_loaded": 4,
         "on_member_join": 5,
         "on_member_ban": 5,
         "on_member_remove": 5,
