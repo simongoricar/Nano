49306ce7d86193636dbd4aeab1bc484342f2f8c9
DefaltSimon <simon.peter.goricar@gmail.com>
2018-02-19 12:08:21 +0100

    Update docker-build

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -3,9 +3,19 @@
 # Go to the "main" directory
 cd ..
 
+# Find bot version
 VERSION=$(python ./utilities/find_version.py)
-echo "Detected version: $VERSION"
+
+if [[ $1 ]];
+then
+  TAG=$1
+  echo "Using tag: ${TAG}"
+else
+  echo "No argument passed, using Nano version."
+  TAG=$VERSION
+  echo "Using tag: ${TAG}"
+fi
 
 echo "Building..."
-docker build -t nano:$VERSION . --build-arg VERSION=$VERSION
+docker build -t nano:${TAG} . --build-arg VERSION=${VERSION}
 echo "DONE!"
