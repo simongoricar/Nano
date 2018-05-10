369fcf80a239a7c1fea16288e1b5489af90ec5c2
DefaltSimon <simon.peter.goricar@gmail.com>
2018-05-10 17:48:56 +0200

    Version bump to 3.8.1

--- startbot.bat
+++ startbot.bat
@@ -1,5 +1,5 @@
 @echo off
 @title Nano
 
-# Make sure to adapt your python version below (linux - python3, python3.5)
+# Make sure to adapt your python version below
 python nano.py
--- startbot.sh
+++ startbot.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Backups the log file (true returned in case my.log does not exist yet)
-cp my.log my.log.bak | true
-# Removes the old log file
-rm -f my.log
-
-# Starts Nano with nohup - output is redirected to my.log
-nohup python3 nano.py > my.log 2>&1&
-
-# In case you need to kill the process, save_pid.txt is saved
-echo $! > save_pid.txt
+screen -d -m -L -S Nano  python3 nano.py
